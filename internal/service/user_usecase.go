400e78ad84272849b6154b8fe4c19bffa1996ada
Duc Nguyen <minhduc140583@gmail.com>
2023-07-16 23:30:53 +0700

    Apply new version

--- internal/service/user_usecase.go
+++ internal/service/user_usecase.go
@@ -7,15 +7,15 @@ import (
 	. "go-service/internal/model"
 )
 
-func NewUserService(repository Repository) *UserUsecase {
-	return &UserUsecase{repository: repository}
+func NewUserService(repository Repository) *UserUseCase {
+	return &UserUseCase{repository: repository}
 }
 
-type UserUsecase struct {
+type UserUseCase struct {
 	repository Repository
 }
 
-func (s *UserUsecase) Load(ctx context.Context, id string) (*User, error) {
+func (s *UserUseCase) Load(ctx context.Context, id string) (*User, error) {
 	var user User
 	ok, err := s.repository.LoadAndDecode(ctx, id, &user)
 	if !ok {
@@ -24,12 +24,12 @@ func (s *UserUsecase) Load(ctx context.Context, id string) (*User, error) {
 		return &user, err
 	}
 }
-func (s *UserUsecase) Create(ctx context.Context, user *User) (int64, error) {
+func (s *UserUseCase) Create(ctx context.Context, user *User) (int64, error) {
 	return s.repository.Insert(ctx, user)
 }
-func (s *UserUsecase) Update(ctx context.Context, user *User) (int64, error) {
+func (s *UserUseCase) Update(ctx context.Context, user *User) (int64, error) {
 	return s.repository.Update(ctx, user)
 }
-func (s *UserUsecase) Delete(ctx context.Context, id string) (int64, error) {
+func (s *UserUseCase) Delete(ctx context.Context, id string) (int64, error) {
 	return s.repository.Delete(ctx, id)
 }
